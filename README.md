502889a97bda9e0bcf2a0a878107a192e7e1d169
VadymDenysiuk <80970410371q@gmail>
2022-04-13 11:20:41 +0300

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # goit-node-hw-01
+
+https://ibb.co/hmjMGcF
+https://ibb.co/Hd2FFgL
+https://ibb.co/WDWQsqN
+https://ibb.co/PZ4ynYG
---
+++ contacts.js
@@ -0,0 +1,47 @@
+const fs = require ("fs/promises");
+const path = require ("path");
+const { v4 } = require ("uuid");
+
+const contactsPath = path.join("./db/contacts.json")
+
+
+async function listContacts () {
+    const data = await fs.readFile(contactsPath);
+    const contacts = JSON.parse(data);
+    return contacts;
+  }
+  
+async function getContactById(contactId) {
+    const contacts = await listContacts();
+    const result = contacts.find(contact =>  contact.id === contactId);
+    if(!result) {
+        return null;
+    }
+    return result;
+  }
+  
+async function removeContact(contactId) {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(contact => contact.id === contactId)
+    if(idx === -1) {
+      return null
+    }
+    const [removedContact] = contacts.splice(idx, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts))
+    return removedContact;
+  }
+  
+async function addContact(name, email, phone) {
+    const contacts = await listContacts();
+    const newContact = {name, email, phone, id: v4() }
+    contacts.push(newContact)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts))
+    return newContact;
+  }
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+} 
